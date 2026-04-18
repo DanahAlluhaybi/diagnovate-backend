6fcc9f0d3a9bc8c2a2256ea84ab42874b142c8c6
Dana <danahalluhaybi@gmail.com>
2026-04-18 21:10:30 +0300

    fix: dynamic port binding

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5002)))
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
